397b0f9a7a5ad106567e56e8bfe4abf93ac911d2
Md Amin Islam <142400970+Programmeramin@users.noreply.github.com>
2024-03-24 15:20:39 +0600

    graphql server done

---
+++ db.js
@@ -0,0 +1,58 @@
+export const products = [
+  {
+    id : 1,
+    name : "Iphone 15 pro max",
+    price : 1500
+  },
+
+  {
+    id : 2,
+    name : "Macbook pro",
+    price : 2500,
+  },
+  {
+    id : 3,
+    name : "Samsung s24 ultra",
+    price : 1200
+  },
+]
+
+
+export const customers = [
+  {
+    id : 1,
+    name : "Ripon mia",
+    age : 23,
+    location : "Mirpur",
+    isMarried : false,
+    gender : "Male"
+  },
+
+  {
+    id : 1,
+    name : "Uttam Roy",
+    age : 32,
+    location : "Uttara",
+    isMarried : true,
+    gender : "Male"
+  },
+
+  {
+    id : 1,
+    name : "Ashik Islam",
+    age : 23,
+    location : "Uttara",
+    isMarried : false,
+    gender : "Male"
+  },
+
+  {
+    id : 1,
+    name : "Jamila Khatun",
+    age : 52,
+    location : "Mirpur",
+    isMarried : false,
+    gender : "Female"
+  },
+  
+]
